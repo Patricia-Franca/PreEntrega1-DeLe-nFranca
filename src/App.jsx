99b586e227bcccee19169ba5f0daff6fac38c4c0
Patricia-Franca <patricia.deleon71@outlook.com>
2022-11-26 12:37:17 -0300

    agregando colores

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import Navbar from './Components/Navbar'; 
+import Navbar1 from './Components/Navbar1'; 
 import ItemListContainer from './Components/ItemListContainer';
 import './App.css'
 const App = () => {
-  let name = "Patri"
+  let name = "World"
   return (
     <div>
-   <Navbar /> 
+   <Navbar1/> 
    <ItemListContainer mensaje="Sector debajo del Navbar"/>
     <h1> Helloo {name} </h1>
 
